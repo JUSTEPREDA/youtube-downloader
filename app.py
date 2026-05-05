55bb636a564b81b23676e7f7267881bb973521b6
JUSTEPREDA <martialnathan0@gmail.com>
2026-05-05 18:10:22 +0200

    fix render port

--- app.py
+++ app.py
@@ -136,4 +136,5 @@ def get_file(job_id):
     return send_file(filepath, as_attachment=True, download_name=filename)
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000, debug=False)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host='0.0.0.0', port=port, debug=False)
